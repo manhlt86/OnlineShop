fca546af80b2353b958ccd3355ee85efa8a94cc6
your name <letranmanh8x@gmail.com>
2021-08-13 09:26:05 +0700

    Upload css admin

---
+++ Models/Services/AccountService.cs
@@ -0,0 +1,31 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Services
+{
+    public class AccountService
+    {
+        private OnlineShopDbContext contex = null;
+
+        public AccountService()
+        {
+            contex = new OnlineShopDbContext();
+        }
+
+        public bool Login(string userName, string password)
+        {
+            object[] sqlParams =
+            {
+                new SqlParameter("@UserName", userName),
+                new SqlParameter("@Password", password)
+            };
+            bool res = contex.Database.SqlQuery<bool>("sp_Account_Login @UserName, @Password", sqlParams).SingleOrDefault();
+            return res;
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Models.Services;
+using OnlineShop.Areas.Admin.Code;
+using OnlineShop.Areas.Admin.Models;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            //bool result = new AccountService().Login(model.UserName, model.Password);
+            if (Membership.ValidateUser(model.UserName, model.Password) && ModelState.IsValid)
+            {
+                //SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+                FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
